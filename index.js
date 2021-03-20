e89e0a761cec5de4ff67307c5f9d7f89793d6ba2
Beelthazad <eduardocgcadiz@gmail.com>
2021-03-20 19:28:03 +0000

    index.js /hello with GET/POST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cool = require("cool-ascii-faces");
 var express = require("express");
 var serveIndex = require('serve-index');
+// var path = require("path");
 var app = express();
 
 var port = 11337;
@@ -15,10 +16,20 @@ function hehe() {
 	return res;
 }
 
-/* La carpeta public estará disponible en /public */
+// app.use("/", express.static(path.join(_dirname,"public")));
+/* La carpeta public estarรก disponible en /public */
 app.use('/public', serveIndex('public')); // shows you the file list
 app.use('/public', express.static('public')); // serve the actual files
 /*----------------------------------------------- */
+app.get('/hello', (req, res) => {
+	res.send("<html><body><h1> Hello! From tiny express server!</h1></body></html>");
+	console.log("New GET request to /hello has arrived");
+})
+
+app.post('/hello', (req, res) => {
+	res.send("<html><body><h1> Hello! From tiny express server!</h1></body></html>");
+	console.log("New POST request to /hello has arrived");
+})
 
 app.get("/",(request, response) => {
 	response.send("<h1>Welcome to SOS2021-23</h1><br>" + cool());
