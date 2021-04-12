10ed1809fb9190dda11b0f30f1bc22c5c77ef865
Beelthazad <eduardocgcadiz@gmail.com>
2021-04-12 16:40:54 +0200

    Fix on auxiliary functions for mh-stats API

--- mh-stats-api/index.js
+++ mh-stats-api/index.js
@@ -1,5 +1,20 @@
 var BASE_API_PATH_EDU="/api/v1/mh-stats";
 const fs = require('fs');
+
+function isAO(val) {
+    return val instanceof Array || val instanceof Object ? true : false;
+}
+
+function elementExists(obj, obj_t) {
+	for (var i = 0; i < obj.length; i++) {
+		if (obj[i] == obj_t) {
+			return true;
+		} else {
+			false;
+		}
+	}
+}
+
 module.exports.register = (app) => {
     var mh_countries = [];
     app.get(BASE_API_PATH_EDU+"/loadInitialData", (request, response) =>{
